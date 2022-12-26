5bbc4b30c409ac97241988e6315503e84bc9ddd8
alfiejfs <alfie@alfiesmith.net>
2022-12-26 22:42:37 +0000

    Pull proper daily problem

--- src/leetcode/api.js
+++ src/leetcode/api.js
@@ -1,49 +1,38 @@
 const { LeetCode } = require("leetcode-query");
 
 const leetcode = new LeetCode();
-let problems = [];
-
-async function init() {
-  problems = await loadProblems();
-}
 
 async function getUser(username) {
   const user = await leetcode.user(username);
   return user;
 }
 
-async function getRandomProblem() {
-  return leetcode.problem(
-    problems[Math.floor(Math.random() * problems.length)].titleSlug
-  );
-}
+async function getDailyProblem() {
+  const { data } = await leetcode.graphql({
+    operationName: "daily",
+    query: `
+      query daily {
+        challenge: activeDailyCodingChallengeQuestion {
+            date
+            link
+            question {
+                difficulty
+                id: questionFrontendId
+                title
+                slug: titleSlug
+                tags: topicTags {
+                    name
+                    slug
+                }
+            }
+        }
+    }`,
+  });
 
-async function getProblemByTitle(title) {
-  return leetcode.problem(
-    problems.filter(problem => problem.title === title)[0].titleSlug
-  );
-}
-
-async function loadProblems() {
-  const easyProblems = await fetchProblems(50, "EASY");
-  const mediumProblems = await fetchProblems(50, "MEDIUM");
-  const hardProblems = await fetchProblems(25, "HARD");
-  return easyProblems.questions.concat(
-    mediumProblems.questions,
-    hardProblems.questions
-  );
-}
-
-async function fetchProblems(limit, difficulty) {
-    return await leetcode.problems({
-        limit: limit,
-        filters: { difficulty: difficulty }
-    });
+  return leetcode.problem(data.challenge.question.slug);
 }
 
 module.exports = {
-  init,
   getUser,
-  getRandomProblem,
-  getProblemByTitle
+  getDailyProblem,
 };
